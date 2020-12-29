dcd0fbf58901b1573c7c214bab2b61c33e2aa24c
VitPap <44554382+VitPap@users.noreply.github.com>
2020-12-29 16:30:46 +0300

    Moving Update 2.5
    Addings:
    1)Tower Moving. Now you can move the tower!

--- Assets/Scripts/Moving/TowerMoving.cs
+++ Assets/Scripts/Moving/TowerMoving.cs
@@ -4,10 +4,56 @@ public class TowerMoving : MonoBehaviour
 {
     public Tank TankObj; // tank gameobject
 
-    private GameObject Tower; // tower of tank
+    public GameObject Tower; // tower of tank
 
+    private float Rotate = 0; 
+    private string State = "Stop"; // currect state of tower
     private void Update()
     {
-        
+        #region TowerMoving 
+        if (Input.GetKeyDown("e"))
+            State = "Right";
+
+        if (Input.GetKeyUp("e"))
+            State = "Stop";
+
+        if (Input.GetKeyDown("q"))
+            State = "Left";
+
+        if (Input.GetKeyUp("q"))
+            State = "Stop";
+
+        if (State == "Stop") // Stopping Tower
+        {
+            if (Rotate < 0)
+            {
+                Rotate += TankObj.TowerParams.Boost;
+
+                if (Rotate > 0) Rotate = 0;
+            }
+            else if (Rotate > 0)
+            {
+                Rotate -= TankObj.TowerParams.Boost;
+
+                if (Rotate < 0) Rotate = 0;
+            }
+        }
+        else if (State == "Right") // Rotate to Right
+        {
+            Rotate += TankObj.TowerParams.Boost;
+
+            if (Rotate > TankObj.TowerParams.MaxSpeed)
+                Rotate = TankObj.TowerParams.MaxSpeed;
+        } else if (State == "Left") // Rotate to Left
+        {
+            Rotate -= TankObj.TowerParams.Boost;
+
+            if (Rotate < -TankObj.TowerParams.MaxSpeed)
+                Rotate = -TankObj.TowerParams.MaxSpeed;
+        }
+
+        Tower.transform.Rotate(Vector3.forward, -Rotate);
+
+        #endregion
     }
 }
